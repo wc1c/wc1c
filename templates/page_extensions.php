8cefdc45cb4b8de0616174532cadbf69d6af069c
Oleg Budrin <ru.mofsy@yandex.ru>
2020-05-06 15:28:04 +0300

    Fix: more

--- templates/page_extensions.php
+++ templates/page_extensions.php
@@ -2,10 +2,10 @@
 
 <div class="wrap">
 
-	<?php do_action('wc1c_admin_before_extensions_show'); ?>
+	<?php do_action('wc1c_admin_extensions_before_show'); ?>
 
 	<?php do_action('wc1c_admin_extensions_show'); ?>
 
-	<?php do_action('wc1c_admin_after_extensions_show'); ?>
+	<?php do_action('wc1c_admin_extensions_after_show'); ?>
 
 </div>
