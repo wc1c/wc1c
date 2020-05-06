9175e2c98aa421da00b56460be6a46f288d7b003
wc1c-ceo <ceo@wc1c.info>
2020-05-06 12:46:38 +0300

    Fix: more

--- templates/report_item.php
+++ templates/report_item.php
@@ -2,7 +2,7 @@
 
 <div class="card col-24 mb-3 pt-0 pb-0 pl-0 pr-0 border-0" style="border-radius: 0!important;">
     <div class="card-header p-2">
-        <h4 class="m-0 p-0"><?php _e($args['title']); ?></h4>
+        <h4 class="m-0 p-0 pb-1"><?php _e($args['title']); ?></h4>
     </div>
     <div class="card-body p-0">
         <table class="table  m-0 table-bordered">
