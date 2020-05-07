e7691b486f37f24fb3bb7234f98589be507d52df
wc1c-ceo <ceo@wc1c.info>
2020-05-07 03:32:54 +0300

    Fix: more

--- includes/abstracts/abstract-class-wc1c-tool.php
+++ includes/abstracts/abstract-class-wc1c-tool.php
@@ -16,6 +16,32 @@ abstract class Wc1c_Abstract_Tool
 	private $id = '';
 
 	/**
+	 * Current version
+	 *
+	 * @var string
+	 */
+	private $version = '';
+
+	/**
+	 * Name
+	 *
+	 * @var string
+	 */
+	private $name = '';
+
+	/**
+	 * Description
+	 *
+	 * @var string
+	 */
+	private $description = '';
+
+	/**
+	 * @var string
+	 */
+	private $author = 'WC1C team';
+
+	/**
 	 * Set tool id
 	 *
 	 * @param $id
@@ -37,5 +63,70 @@ abstract class Wc1c_Abstract_Tool
 	public function get_id()
 	{
 		return $this->id;
+	}
+
+
+	/**
+	 * @return string
+	 */
+	public function get_name()
+	{
+		return $this->name;
+	}
+
+	/**
+	 * @param string $name
+	 */
+	public function set_name($name)
+	{
+		$this->name = $name;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function get_description()
+	{
+		return $this->description;
+	}
+
+	/**
+	 * @param string $description
+	 */
+	public function set_description($description)
+	{
+		$this->description = $description;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function get_author()
+	{
+		return $this->author;
+	}
+
+	/**
+	 * @param string $author
+	 */
+	public function set_author($author)
+	{
+		$this->author = $author;
+	}
+	
+	/**
+	 * @return string
+	 */
+	public function get_version()
+	{
+		return $this->version;
+	}
+
+	/**
+	 * @param string $version
+	 */
+	public function set_version($version)
+	{
+		$this->version = $version;
 	}
 }
