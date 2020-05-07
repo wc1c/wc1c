767dadee4d5f258e299cd766c5661fecb0616007
Oleg Budrin <ru.mofsy@yandex.ru>
2020-05-07 10:53:18 +0300

    Fix: more

---
+++ templates/tools_item.php
@@ -0,0 +1,15 @@
+<?php defined('ABSPATH') || exit; ?>
+
+<div class="card col-24 mb-2 mt-2 pt-0 pb-0 pl-0 pr-0 border-0" style="border-radius: 0!important;">
+    <div class="card-header p-2">
+        <span class="badge badge-info p-2" style="font-size: 1em;"> Version: <?php echo $args['object']->get_version(); ?></span>
+        <span class="badge badge-info p-2" style="font-size: 1em;">ID: <?php echo $args['id']; ?></span>
+    </div>
+    <div class="card-body p-3">
+        <h5 class="card-title mt-0"><?php echo $args['object']->get_name(); ?></h5>
+        <p class="card-text"><?php echo $args['object']->get_description(); ?></p>
+    </div>
+    <div class="card-footer">
+        Author: <?php echo $args['object']->get_author(); ?>
+    </div>
+</div>
