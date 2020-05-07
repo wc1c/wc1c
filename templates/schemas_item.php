78c66d87fc00aa14f385d978d7960fc8312077c9
wc1c-ceo <ceo@wc1c.info>
2020-05-07 12:03:12 +0300

    Fix: more

--- templates/schemas_item.php
+++ templates/schemas_item.php
@@ -3,7 +3,7 @@
 <div class="card col-24 mb-2 mt-2 pt-0 pb-0 pl-0 pr-0 border-0" style="border-radius: 0!important;">
     <div class="card-header p-2">
         <span class="badge badge-info p-2" style="font-size: 1em;"> Version: <?php echo $args['object']->get_version(); ?></span>
-        <span class="badge badge-info p-2" style="font-size: 1em;">ID: <?php echo $args['id']; ?></span>
+        <span class="badge badge-info p-2" style="font-size: 1em;">Schema ID: <?php echo $args['id']; ?></span>
     </div>
     <div class="card-body p-3">
         <h5 class="card-title mt-0"><?php echo $args['object']->get_name(); ?></h5>
