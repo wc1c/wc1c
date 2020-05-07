0b3d51536fad33461bb782dd60132a813c758ea1
wc1c-ceo <ceo@wc1c.info>
2020-05-07 14:08:33 +0300

    Fix: more

--- templates/tools_header.php
+++ templates/tools_header.php
@@ -1,3 +1,17 @@
 <?php defined('ABSPATH') || exit; ?>
 
-<p><?php _e('Information about tools installed in the system.', 'wc1c') ?></p>
+<?php
+if(wc1c_get_var($_GET['tool_id'], '') !== '' && is_wc1c_admin_tools_request())
+{
+
+}
+else
+{
+    ?>
+
+    <p><?php _e('Information about tools installed in the system.', 'wc1c') ?></p>
+
+<?php
+}
+?>
+
