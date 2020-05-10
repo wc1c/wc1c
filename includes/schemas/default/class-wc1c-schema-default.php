1766697e7afcf3957769df62330972d2d500e3b0
Oleg Budrin <ru.mofsy@yandex.ru>
2020-05-10 13:54:41 +0300

    Fix: more

--- includes/schemas/default/class-wc1c-schema-default.php
+++ includes/schemas/default/class-wc1c-schema-default.php
@@ -112,7 +112,7 @@ class Wc1c_Schema_Default extends Wc1c_Abstract_Schema
 	 *
 	 * @return array
 	 */
-	public function configuration_fields($fields = array())
+	public function configuration_fields($fields = [])
 	{
 		$fields['main'] = array
 		(
