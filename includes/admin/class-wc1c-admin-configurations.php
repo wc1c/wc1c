8e8ea0fcd028e4b509fe911c32ccb92ce9ea7219
wc1c-ceo <ceo@wc1c.info>
2020-05-12 19:35:16 +0300

    Fix: more

--- includes/admin/class-wc1c-admin-configurations.php
+++ includes/admin/class-wc1c-admin-configurations.php
@@ -48,13 +48,6 @@ class Wc1c_Admin_Configurations
 		 */
 		if('list' === $this->get_current_action())
 		{
-			if(!class_exists('Wc1c_Admin_Abstract_Table'))
-			{
-				include_once WC1C_PLUGIN_PATH . 'includes/abstracts/abstract-class-wc1c-admin-table.php';
-			}
-
-			include_once WC1C_PLUGIN_PATH . 'includes/admin/class-wc1c-admin-configurations-table.php';
-
 			/**
 			 * Table
 			 */
@@ -105,6 +98,8 @@ class Wc1c_Admin_Configurations
 	 */
 	public function includes()
 	{
+		include_once WC1C_PLUGIN_PATH . 'includes/abstracts/abstract-class-wc1c-admin-table.php';
+		include_once WC1C_PLUGIN_PATH . 'includes/admin/class-wc1c-admin-configurations-table.php';
 		include_once WC1C_PLUGIN_PATH . 'includes/admin/class-wc1c-admin-configurations-create.php';
 		include_once WC1C_PLUGIN_PATH . 'includes/admin/class-wc1c-admin-configurations-update.php';
 	}
