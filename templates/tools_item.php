988d737ae58973fa5db869c59fd35e31ee32d375
Oleg Budrin <ru.mofsy@yandex.ru>
2020-05-12 16:39:40 +0300

    Fix: more

--- templates/tools_item.php
+++ templates/tools_item.php
@@ -2,10 +2,16 @@
 
 <div class="card col-24 mb-2 mt-2 pt-0 pb-0 pl-0 pr-0 border-0" style="border-radius: 0!important;">
     <div class="card-body p-3">
-        <h5 class="card-title mt-0"><?php echo $args['object']->get_name(); ?></h5>
-        <p class="card-text"><?php echo $args['object']->get_description(); ?></p>
+        <h5 class="card-title mt-0">
+            <?php echo $args['object']->get_name(); ?>
+        </h5>
+        <p class="card-text">
+            <?php echo $args['object']->get_description(); ?>
+        </p>
     </div>
     <div class="card-footer p-3">
-       <a class="btn text-white btn-success" href="<?php echo get_wc1c_admin_tools_url($args['object']->get_id())?>">Open</a>
+       <a class="btn text-white btn-success" href="<?php echo get_wc1c_admin_tools_url($args['object']->get_id())?>">
+	       <?php _e('Open', 'wc1c'); ?>
+       </a>
     </div>
 </div>
