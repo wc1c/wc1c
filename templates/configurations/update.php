2f722abd66151c3812d10a23365e4ed3e42b08c7
wc1c-ceo <ceo@wc1c.info>
2020-05-14 22:21:20 +0300

    Fix: more

--- templates/configurations/update.php
+++ templates/configurations/update.php
@@ -2,6 +2,13 @@
 
 <?php do_action('wc1c_admin_before_configurations_update_show'); ?>
 
-<?php do_action('wc1c_admin_configurations_update_show'); ?>
+<div class="row">
+	<div class="col-18">
+		<?php do_action('wc1c_admin_configurations_update_show'); ?>
+	</div>
+	<div class="col-6">
+		<?php do_action('wc1c_admin_configurations_update_sidebar_show'); ?>
+	</div>
+</div>
 
 <?php do_action('wc1c_admin_after_configurations_update_show'); ?>
