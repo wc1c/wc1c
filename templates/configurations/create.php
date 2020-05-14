8aad1cba4ee37d4ca7e172729d68f283f57f8b58
Oleg Budrin <ru.mofsy@yandex.ru>
2020-05-14 22:31:24 +0300

    Fix: more

--- templates/configurations/create.php
+++ templates/configurations/create.php
@@ -1,15 +1,15 @@
 <?php defined('ABSPATH') || exit; ?>
 
 <div class="row">
-    <div class="col-24 col-lg-16">
+    <div class="col-24 col-lg-18 p-0">
 
-        <h2><?php _e('Create new configuration', 'wc1c'); ?></h2>
+        <h2 class="pt-0 mt-0"><?php _e('Create new configuration', 'wc1c'); ?></h2>
 
         <p><?php _e('The established exchange schemes are used to create the configuration.', 'wc1c'); ?></p>
 
         <?php do_action('wc1c_admin_configurations_create_show'); ?>
     </div>
-    <div class="col-24 col-lg-7">
+    <div class="col-24 col-lg-6 p-0">
         <div class="card m-0 p-1" style="width: 100%;max-width: 100%;">
             <ul class="list-group m-0 list-group-flush">
                 <li class="list-group-item"><?php _e('1. Enter a convenient name.', 'wc1c'); ?></li>
