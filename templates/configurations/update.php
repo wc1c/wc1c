7c18382b0792012868272295a3f785f9ccb3ce19
Oleg Budrin <ru.mofsy@yandex.ru>
2020-05-14 22:55:55 +0300

    Fix: more

--- templates/configurations/update.php
+++ templates/configurations/update.php
@@ -3,10 +3,10 @@
 <?php do_action('wc1c_admin_before_configurations_update_show'); ?>
 
 <div class="row m-0">
-	<div class="col-18 p-0">
+	<div class="col-17 p-0">
 		<?php do_action('wc1c_admin_configurations_update_show'); ?>
 	</div>
-	<div class="col-6 p-0">
+	<div class="col-7 p-0">
 		<?php do_action('wc1c_admin_configurations_update_sidebar_show'); ?>
 	</div>
 </div>
