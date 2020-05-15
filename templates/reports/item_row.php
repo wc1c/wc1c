8a632224b716b7c915a8c0136db0a634642a45bd
Oleg Budrin <ru.mofsy@yandex.ru>
2020-05-15 17:37:52 +0300

    Fix: more

--- templates/reports/item_row.php
+++ templates/reports/item_row.php
@@ -3,12 +3,12 @@
 <tr class="">
     <td class="" style="width: 40%;">
 	    <?php
-	    echo esc_html__($args['title']);
+	    esc_html_e($args['title']);
 	    ?>
     </td>
     <td class="">
 	    <?php
-	    echo esc_html__(apply_filters('wc1c_admin_report_data_row_print', $args['data']));
+	    esc_html_e(apply_filters('wc1c_admin_report_data_row_print', $args['data']));
 	    ?>
     </td>
 </tr>
