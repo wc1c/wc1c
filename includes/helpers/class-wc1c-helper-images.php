89bdaac318ef33094260b9a14453f69c8c777da6
wc1c-ceo <ceo@wc1c.info>
2020-05-20 01:49:58 +0300

    Init: helpers #2

---
+++ includes/helpers/class-wc1c-helper-images.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Images class
+ *
+ * @package Wc1c
+ */
+defined('ABSPATH') || exit;
+
+class Wc1c_Helper_Images
+{
+	/**
+	 * Wc1c_Helper_Images constructor
+	 */
+	public function __construct()
+	{
+	}
+}
---
+++ includes/helpers/class-wc1c-helper-products.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Products class
+ *
+ * @package Wc1c
+ */
+defined('ABSPATH') || exit;
+
+class Wc1c_Helper_Products
+{
+	/**
+	 * Wc1c_Helper_Products constructor
+	 */
+	public function __construct()
+	{
+	}
+}
