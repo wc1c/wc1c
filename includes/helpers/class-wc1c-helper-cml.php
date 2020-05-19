96cff71b586de7c3710510a0b1245cde6c632eb5
Oleg Budrin <ru.mofsy@yandex.ru>
2020-05-20 01:40:02 +0300

    Init: helpers

---
+++ includes/helpers/class-wc1c-helper-cml.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Helper class
+ *
+ * @package Wc1c
+ */
+defined('ABSPATH') || exit;
+
+class Wc1c_Helper_Cml
+{
+	/**
+	 * Wc1c_Helper_Cml constructor
+	 */
+	public function __construct()
+	{
+	}
+
+	/**
+	 * Определение типа файла
+	 *
+	 * @param $file_name
+	 *
+	 * @return string
+	 */
+	private function file_type_detect($file_name)
+	{
+		$types = array('import', 'offers', 'prices', 'rests', 'import_files');
+		foreach($types as $type)
+		{
+			$pos = stripos($file_name, $type);
+			if($pos !== false)
+			{
+				return $type;
+			}
+		}
+		return '';
+	}
+}
