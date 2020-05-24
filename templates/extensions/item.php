7a2b6d825a947173f1717d53eca3ec17cd7a494f
wc1c-ceo <ceo@wc1c.info>
2020-05-24 15:21:06 +0300

    Fix: more

--- templates/extensions/item.php
+++ templates/extensions/item.php
@@ -2,22 +2,27 @@
 
 <div class="card col-24 mb-2 mt-2 pt-0 pb-0 pl-0 pr-0 border-0" style="border-radius: 0!important;">
     <div class="card-header p-2">
-        <span class="badge badge-info p-2" style="font-size: 1em;">
-            <?php _e('Version:', 'wc1c'); ?> <?php echo $args['object']->get_meta('version', 'none'); ?>
-        </span>
-        <span class="badge badge-info p-2" style="font-size: 1em;">
-            <?php _e('Extension ID:', 'wc1c'); ?> <?php echo $args['id']; ?>
-        </span>
-    </div>
-    <div class="card-body p-3">
-        <h5 class="card-title mt-0">
-            <?php echo $args['object']->get_meta('name', 'none'); ?>
+        <h5 class="card-title mt-0 mb-0">
+	        <?php echo $args['object']->get_meta('name', 'none'); ?>
         </h5>
-        <p class="card-text">
+    </div>
+    <div class="card-body p-2 pt-3 pb-3">
+        <p class="card-text mt-0">
             <?php echo $args['object']->get_meta('description', 'none'); ?>
         </p>
     </div>
     <div class="card-footer p-2">
-        <?php _e('Author:', 'wc1c'); ?> <?php echo $args['object']->get_meta('author', 'none'); ?>
+        <?php _e('Author:', 'wc1c'); ?>
+        <span class="badge badge-info p-1" style="font-size: 1em;">
+              <?php echo $args['object']->get_meta('author', 'none'); ?>
+        </span>
+	    <?php _e('Version:', 'wc1c'); ?>
+        <span class="badge badge-info p-1" style="font-size: 1em;">
+             <?php echo $args['object']->get_meta('version', 'none'); ?>
+        </span>
+        <?php _e('Extension ID:', 'wc1c'); ?>
+        <span class="badge badge-info p-1" style="font-size: 1em;">
+             <?php echo $args['id']; ?>
+        </span>
     </div>
 </div>
