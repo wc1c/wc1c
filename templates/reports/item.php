c3b6981992bc37896905f615e3a8a4f495563c93
Oleg Budrin <ru.mofsy@yandex.ru>
2020-05-24 18:55:36 +0300

    Fix: more

--- templates/reports/item.php
+++ templates/reports/item.php
@@ -2,7 +2,7 @@
 
 <div class="card col-24 mb-2 mt-2 pt-0 pb-0 pl-0 pr-0 border-0" style="border-radius: 0!important;">
     <div class="card-header p-2">
-        <h4 class="m-0 p-0 pb-1"><?php _e($args['title']); ?></h4>
+        <h5 class="m-0 p-0 pb-1"><?php _e($args['title']); ?></h5>
     </div>
     <div class="card-body p-0">
         <table class="table  m-0 table-bordered">
