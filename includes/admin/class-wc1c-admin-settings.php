8fec9d43aea3911de484ae043d15f8eb426fd2a0
Oleg Budrin <ru.mofsy@yandex.ru>
2020-05-29 10:46:55 +0300

    Fix: logger description

--- includes/admin/class-wc1c-admin-settings.php
+++ includes/admin/class-wc1c-admin-settings.php
@@ -234,9 +234,9 @@ class Wc1c_Admin_Settings extends Wc1c_Admin_Abstract_Form
 
 		$fields['logger'] = array
 		(
-			'title' => __('Logging level', 'wc1c'),
+			'title' => __('Logging events', 'wc1c'),
 			'type' => 'select',
-			'description' => __('You can enable logging, specify the level of error that you want to benefit from logging. You can send reports to developer manually by pressing the button. All sensitive data in the report are deleted. By default, the error rate should not be less than ERROR.', 'wc1c') . '<br/><b>' . __('Current file: ', 'wc1c') . '</b>' . $logger_path,
+			'description' => __('All events of the selected level will be recorded in the log file. The higher the level, the less data is recorded.', 'wc1c') . '<br/><b>' . __('Current file: ', 'wc1c') . '</b>' . $logger_path,
 			'default' => '400',
 			'options' => array
 			(
