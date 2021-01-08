5494584374b8ee4ad657055b07c306cb9e4f37be
wc1c-ceo <ceo@wc1c.info>
2021-01-09 00:20:01 +0300

    Fix: more

--- templates/configurations/update_form.php
+++ templates/configurations/update_form.php
@@ -2,10 +2,12 @@
 
 <form method="post" action="">
 	<?php wp_nonce_field('wc1c-admin-configurations-update-save', '_wc1c-admin-nonce'); ?>
-    <table class="form-table wc1c-admin-form-table">
-        <?php $args['object']->generate_html($args['object']->get_fields(), true); ?>
-    </table>
+    <div class="bg-white p-1">
+        <table class="form-table wc1c-admin-form-table">
+            <?php $args['object']->generate_html($args['object']->get_fields(), true); ?>
+        </table>
+    </div>
     <p class="submit">
-	<input type="submit" name="submit" id="submit" class="button button-primary" value="<?php _e('Save configuration', 'wc1c'); ?>">
+	    <input type="submit" name="submit" id="submit" class="button button-primary" value="<?php _e('Save configuration', 'wc1c'); ?>">
     </p>
 </form>
