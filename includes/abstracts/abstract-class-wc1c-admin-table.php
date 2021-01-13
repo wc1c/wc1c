dc1923da43d87ae4d81ac3df7a0925a3d1097daf
Oleg Budrin <ru.mofsy@yandex.ru>
2021-01-13 13:06:42 +0300

    Fix: more

--- includes/abstracts/abstract-class-wc1c-admin-table.php
+++ includes/abstracts/abstract-class-wc1c-admin-table.php
@@ -349,7 +349,7 @@ abstract class Wc1c_Admin_Abstract_Table
 
 		echo '<label for="bulk-action-selector-' . esc_attr($which) . '" class="screen-reader-text">' . __('Select bulk action') . '</label>';
 		echo '<select name="action' . $two . '" id="bulk-action-selector-' . esc_attr($which) . "\">\n";
-		echo '<option value="-1">' . __('Bulk Actions') . "</option>\n";
+		echo '<option value="-1">' . __('Bulk Actions', 'wc1c') . "</option>\n";
 
 		foreach($this->_actions as $name => $title)
 		{
