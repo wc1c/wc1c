3a22d4368fafd37ed437b3c40b355934a845f674
wc1c-ceo <ceo@wc1c.info>
2021-08-27 22:14:07 +0300

    Fix: more

--- wc1c.php
+++ wc1c.php
@@ -25,8 +25,8 @@ if(false === defined('WC1C_PLUGIN_FILE'))
 	include_once __DIR__ . '/includes/functions-wc1c.php';
 	include_once __DIR__ . '/includes/traits/trait-wc1c-singleton.php';
 	include_once __DIR__ . '/includes/class-wc1c.php';
-}
 
-register_activation_hook(WC1C_PLUGIN_FILE, 'wc1c_install');
+	register_activation_hook(WC1C_PLUGIN_FILE, 'wc1c_install');
+}
 
 add_action('plugins_loaded', 'WC1C', 0);
