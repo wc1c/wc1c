e3b479f8513800e08e92c9e52a03a7936647ff27
wc1c-ceo <ceo@wc1c.info>
2021-09-28 20:17:27 +0300

    WC tested up to: 5.7

--- wc1c.php
+++ wc1c.php
@@ -5,7 +5,7 @@
  * Description: Implementation of a mechanism for flexible exchange of various data between 1C products and a site running WordPress using the WooCommerce plugin.
  * Version: 0.6.0
  * WC requires at least: 3.2
- * WC tested up to: 5.6
+ * WC tested up to: 5.7
  * Text Domain: wc1c
  * Domain Path: /languages
  * Copyright: WC1C team © 2018-2021
