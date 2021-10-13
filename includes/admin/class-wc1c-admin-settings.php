69419d83fa50fc28719da1c7da3ec6b0415f3ff0
wc1c-ceo <ceo@wc1c.info>
2021-10-13 23:04:03 +0300

    Fix: more

--- includes/admin/class-wc1c-admin-settings.php
+++ includes/admin/class-wc1c-admin-settings.php
@@ -9,6 +9,11 @@ defined('ABSPATH') || exit;
 class Wc1c_Admin_Settings
 {
 	/**
+	 * Singleton
+	 */
+	use Trait_Wc1c_Singleton;
+
+	/**
 	 * Wc1c_Admin_Settings constructor
 	 */
 	public function __construct()
@@ -19,17 +24,34 @@ class Wc1c_Admin_Settings
 		$form->init();
 
 		add_action('wc1c_admin_settings_show', [$form, 'output_form'], 10);
+		add_action('wc1c_admin_body_show', [$this, 'output'], 10);
 	}
 
+	/**
+	 * Output page
+	 *
+	 * @return void
+	 */
+	public function output()
+	{
+		wc1c_get_template('settings/page.php');
+	}
+
+	/**
+	 * Load current settings into form
+	 *
+	 * @param $old_data
+	 *
+	 * @return array|mixed
+	 */
 	public function load_data($old_data)
 	{
 		try
 		{
 			$saved_data = WC1C()->settings()->get();
-
 			$old_data = array_merge($old_data, $saved_data);
 		}
-		catch(Exception $e){}
+		catch(Wc1c_Exception_Runtime $e){}
 
 		return $old_data;
 	}
