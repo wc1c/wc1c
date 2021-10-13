6537b700e7fd3fc5482b07e104b562b90ea11cdf
wc1c-ceo <ceo@wc1c.info>
2021-10-13 19:10:12 +0300

    Fix: more

--- includes/class-wc1c-database.php
+++ includes/class-wc1c-database.php
@@ -11,19 +11,4 @@ class Wc1c_Database
 	public function __construct()
 	{
 	}
-
-	public function get_configurations($args = [])
-	{
-
-	}
-
-	public function install_tables()
-	{
-		// todo: move from functions
-	}
-
-	public function check_tables()
-	{
-		// todo: check tables structure for errors
-	}
 }
