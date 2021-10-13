d25354767ac2d75afce5638bc4aac13097ba1087
wc1c-ceo <ceo@wc1c.info>
2021-10-13 22:03:17 +0300

    Fix: more

---
+++ templates/configurations/page.php
@@ -0,0 +1,7 @@
+<?php defined('ABSPATH') || exit; ?>
+
+<?php do_action('wc1c_admin_configurations_before_show'); ?>
+
+<?php do_action('wc1c_admin_configurations_show'); ?>
+
+<?php do_action('wc1c_admin_configurations_after_show'); ?>
