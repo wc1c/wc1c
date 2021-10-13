9d142595e76a5b939b1c05cf66fc107b2be3d782
wc1c-ceo <ceo@wc1c.info>
2021-10-13 16:12:49 +0300

    Init: meta

---
+++ includes/class-wc1c-data-meta.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * Meta
+ *
+ * @package Wc1c
+ */
+defined('ABSPATH') || exit;
+
+class Wc1c_Data_Meta implements JsonSerializable
+{
+	/**
+	 * Current data for metadata
+	 *
+	 * @var array
+	 */
+	protected $current_data;
+
+	/**
+	 * Metadata data
+	 *
+	 * @var array
+	 */
+	protected $data;
+
+	/**
+	 * Constructor
+	 *
+	 * @param array $meta Data to wrap behind this function
+	 */
+	public function __construct($meta = [])
+	{
+		$this->current_data = $meta;
+
+		$this->apply_changes();
+	}
+
+	/**
+	 * When converted to JSON
+	 *
+	 * @return object|array
+	 */
+	public function jsonSerialize()
+	{
+		return $this->get_data();
+	}
+
+	/**
+	 * Merge changes with data and clear.
+	 */
+	public function apply_changes()
+	{
+		$this->data = $this->current_data;
+	}
+
+	/**
+	 * Creates or updates a property in the metadata object
+	 *
+	 * @param string $key Key to set
+	 * @param mixed $value Value to set
+	 */
+	public function __set($key, $value)
+	{
+		$this->current_data[$key] = $value;
+	}
+
+	/**
+	 * Checks if a given key exists in our data. This is called internally
+	 * by `empty` and `isset`
+	 *
+	 * @param string $key Key to check if set
+	 *
+	 * @return bool
+	 */
+	public function __isset($key)
+	{
+		return array_key_exists($key, $this->current_data);
+	}
+
+	/**
+	 * Returns the value of any property
+	 *
+	 * @param string $key Key to get
+	 *
+	 * @return mixed Property value or NULL if it does not exists
+	 */
+	public function __get($key)
+	{
+		if(array_key_exists($key, $this->current_data))
+		{
+			return $this->current_data[$key];
+		}
+
+		return null;
+	}
+
+	/**
+	 * Return data changes only
+	 *
+	 * @return array
+	 */
+	public function get_changes()
+	{
+		$changes = [];
+		foreach($this->current_data as $id => $value)
+		{
+			if(!array_key_exists($id, $this->data) || $value !== $this->data[$id])
+			{
+				$changes[$id] = $value;
+			}
+		}
+
+		return $changes;
+	}
+
+	/**
+	 * Return all data as an array
+	 *
+	 * @return array
+	 */
+	public function get_data()
+	{
+		return $this->data;
+	}
+}
