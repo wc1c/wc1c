04c3658008337d844410b3124644248e0313e13e
wc1c-ceo <ceo@wc1c.info>
2021-12-13 18:22:25 +0300

    Init: CoreLog

---
+++ includes/Log/CoreLog.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Namespace
+ */
+namespace Wc1c\Log;
+
+/**
+ * Only WordPress
+ */
+defined('ABSPATH') || exit;
+
+/**
+ * Dependencies
+ */
+use Monolog\Logger;
+
+/**
+ * Class CoreLog
+ *
+ * @package Wc1c
+ */
+class CoreLog extends Logger
+{
+	/**
+	 * @var string
+	 */
+	protected $name = 'core';
+
+	/**
+	 * CoreLog constructor.
+	 *
+	 * @param array $handlers
+	 * @param array $processors
+	 */
+	public function __construct(array $handlers = [], array $processors = [])
+	{
+		parent::__construct($this->name, $handlers, $processors);
+	}
+}
