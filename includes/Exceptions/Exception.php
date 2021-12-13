78dbc3c479847941e260abb8d0e5fb533f8d6188
wc1c-ceo <ceo@wc1c.info>
2021-12-13 20:05:45 +0300

    Init: Exceptions

---
+++ includes/Exceptions/Exception.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Namespace
+ */
+namespace Wc1c\Exceptions;
+
+/**
+ * Only WordPress
+ */
+defined('ABSPATH') || exit;
+
+/**
+ * Dependencies
+ */
+use Exception as SystemException;
+
+/**
+ * Exception
+ *
+ * @package Wc1c/Exceptions
+ */
+class Exception extends SystemException
+{}
