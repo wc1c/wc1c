19bd356dc2a94ff996255c54510460b3d5c42523
wc1c-ceo <ceo@wc1c.info>
2021-12-14 18:14:12 +0300

    Fix: Configurations create

---
+++ includes/Admin/Configurations/Create.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Namespace
+ */
+namespace Wc1c\Admin\Configurations;
+
+/**
+ * Only WordPress
+ */
+defined('ABSPATH') || exit;
+
+/**
+ * Dependencies
+ */
+use Wc1c\Abstracts\ScreenAbstract;
+use Wc1c\Exceptions\Exception;
+use Wc1c\Traits\Singleton;
+
+/**
+ * Class Create
+ *
+ * @package Wc1c\Admin\Configurations
+ */
+class Create extends ScreenAbstract
+{
+	use Singleton;
+
+	/**
+	 * @throws Exception
+	 */
+	public function __construct()
+	{
+		$form = new CreateForm();
+		$form->load_fields();
+		$form->save();
+
+		add_action(WC1C_ADMIN_PREFIX . 'configurations_form_create_show', [$form, 'output_form'], 10);
+
+		parent::__construct();
+	}
+
+	/**
+	 * Error
+	 */
+	public function wrapError()
+	{
+		wc1c_get_template('configurations/error.php');
+	}
+
+	/**
+	 * Show page
+	 *
+	 * @return void
+	 */
+	public function output()
+	{
+		wc1c_get_template('configurations/create.php');
+	}
+}
