c1feec0b1e3c5c6a9a65b7ad7f02b0d8d45e56f3
wc1c-ceo <ceo@wc1c.info>
2021-12-16 09:40:16 +0300

    Fix: more

--- templates/configurations/update_error.php
+++ templates/configurations/update_error.php
@@ -1,3 +1,13 @@
 <?php defined('ABSPATH') || exit; ?>
 
-<p><?php _e('Error. Update is not available', 'wc1c'); ?></p>
+<?php
+$title = __('Error', 'wc1c');
+$title = apply_filters(WC1C_ADMIN_PREFIX . 'configurations_update_error_title', $title);
+$text = __('Update is not available. Configuration not found or unavailable.', 'wc1c');
+$text = apply_filters(WC1C_ADMIN_PREFIX . 'configurations_update_error_text', $text);
+?>
+
+<div class="wc1c-configurations-alert mb-2">
+    <h3><?php echo $title; ?></h3>
+    <p><?php echo $text; ?></p>
+</div>
--- templates/configurations/update_schema_error.php
+++ templates/configurations/update_schema_error.php
@@ -1,3 +1,13 @@
 <?php defined('ABSPATH') || exit; ?>
 
-<p><?php _e('Error. Update is not available', 'wc1c'); ?></p>
+<?php
+$title = __('Warning', 'wc1c');
+$title = apply_filters(WC1C_ADMIN_PREFIX . 'configurations_update_schema_error_title', $title);
+$text = __('Update is not available.', 'wc1c');
+$text = apply_filters(WC1C_ADMIN_PREFIX . 'configurations_update_schema_error_text', $text);
+?>
+
+<div class="wc1c-configurations-alert mb-2">
+    <h3><?php echo $title; ?></h3>
+    <p><?php echo $text; ?></p>
+</div>
