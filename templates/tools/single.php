00af1c46a4fbb9bdb7345ebed6e4f5bb6cd9f097
wc1c-ceo <ceo@wc1c.info>
2021-12-17 14:26:36 +0300

    Fix: more

--- templates/tools/single.php
+++ templates/tools/single.php
@@ -7,7 +7,7 @@
         <?php
             $label = __('Back to all tools', 'wc1c');
             $url = wc1c_admin_tools_get_url();
-            wc1c_admin_back_link($label, $url);
+            wc1c()->templates()->adminBackLink($label, $url);
         ?>
         <?php do_action(WC1C_ADMIN_PREFIX . 'tools_single_show'); ?>
     </div>
