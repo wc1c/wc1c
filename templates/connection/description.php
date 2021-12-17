bcfc40d0959204ddbf28c9f0a39d148eb373009a
wc1c-ceo <ceo@wc1c.info>
2021-12-17 14:41:56 +0300

    Fix: more

--- templates/connection/description.php
+++ templates/connection/description.php
@@ -3,5 +3,5 @@
 <div class="clearfix"></div>
 
 <div class="mt-2">
-    <p><?php _e('Configuring the site connection to official services and services from the WC1C team.', 'wc1c'); ?></p>
+    <p><?php _e('Configuring the site connection to official services from the WC1C team.', 'wc1c'); ?></p>
 </div>
