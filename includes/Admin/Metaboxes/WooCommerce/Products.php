25e0b7d6df0a8e7adbf5843b5ed5ddb5a1748613
wc1c-ceo <ceo@wc1c.info>
2021-12-19 20:19:43 +0300

    Init: Metaboxes

---
+++ includes/Admin/Metaboxes/WooCommerce/Products.php
@@ -0,0 +1,22 @@
+<?php namespace Wc1c\Admin\Metaboxes\WooCommerce;
+
+defined('ABSPATH') || exit;
+
+use Wc1c\Traits\SingletonTrait;
+
+/**
+ * Products
+ *
+ * @package Wc1c\Admin\Metaboxes\WooCommerce
+ */
+final class Products
+{
+	use SingletonTrait;
+
+	/**
+	 * Products constructor.
+	 */
+	public function __construct()
+	{
+	}
+}
