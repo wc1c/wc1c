01a0285a0559a59cc134166a24fd06a8650b1b6b
wc1c-ceo <ceo@wc1c.info>
2022-01-04 20:26:17 +0300

    Fix: more

--- templates/configurations/empty.php
+++ templates/configurations/empty.php
@@ -2,6 +2,6 @@
 
 <h2><?php esc_html_e( 'Configurations not found.', 'wc1c' ); ?></h2>
 
-<a href="<?php echo $args['url_create']; ?>" class="page-title-action">
+<a href="<?php echo $args['url_create']; ?>" class="mt-2 btn-lg d-inline-block page-title-action">
     <?php _e('New configuration', 'wc1c'); ?>
 </a>
