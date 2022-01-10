865afbf67f35ef4aa8cd349ea81c2391b4865410
wc1c-ceo <ceo@wc1c.info>
2022-01-10 16:51:37 +0300

    Fix: more

--- includes/Connection.php
+++ includes/Connection.php
@@ -2,7 +2,6 @@
 
 defined('ABSPATH') || exit;
 
-use Exception;
 use Digiom\ApClientWP\ApplicationsPasswords;
 use Wc1c\Traits\SingletonTrait;
 
@@ -14,25 +13,4 @@ use Wc1c\Traits\SingletonTrait;
 final class Connection extends ApplicationsPasswords
 {
 	use SingletonTrait;
-
-	/**
-	 * Connection constructor.
-	 *
-	 * @throws Exception
-	 */
-	public function __construct($login = '', $password = '', $use_token = false)
-	{
-		$credentials['login'] = $login;
-
-		if($use_token)
-		{
-			$credentials['token'] = $password;
-		}
-		else
-		{
-			$credentials['password'] = $password;
-		}
-
-		parent::__construct('https://wc1c.info', true, $credentials);
-	}
 }
