bc1de88d2769f41b55ce72c4dae1de49d4b3c52c
wc1c-ceo <ceo@wc1c.info>
2022-01-12 17:08:29 +0300

    Fix: more

--- includes/Loader.php
+++ includes/Loader.php
@@ -33,6 +33,7 @@ final class Loader
 		$this->addNamespace('Digiom\WordPress\Notices', WC1C_PLUGIN_PATH . '/vendor/digiom/notices-wp/src');
 		$this->addNamespace('Digiom\Wap', WC1C_PLUGIN_PATH . '/vendor/digiom/wap/src');
 		$this->addNamespace('Digiom\Psr7wp', WC1C_PLUGIN_PATH . '/vendor/digiom/psr7wp/src');
+		$this->addNamespace('Psr\Http\Message', WC1C_PLUGIN_PATH . '/vendor/psr/http-message/src');
 		$this->addNamespace('Psr\Log', WC1C_PLUGIN_PATH . '/vendor/psr/log/src');
 		$this->addNamespace('Monolog', WC1C_PLUGIN_PATH . '/vendor/monolog/src/Monolog');
 
