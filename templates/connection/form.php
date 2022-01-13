a5a907f1693d8535a481e0046dcd535c6b19d262
wc1c-ceo <ceo@wc1c.info>
2022-01-13 13:30:15 +0300

    Fix: more

--- templates/connection/form.php
+++ templates/connection/form.php
@@ -1,31 +1,30 @@
-<?php defined('ABSPATH') || exit;?>
+<?php defined('ABSPATH') || exit;
+
+use Wc1c\Admin\Settings\ConnectionForm;
+
+/** @var ConnectionForm $object */
+$object = $args['object'];
+
+?>
 
 <form method="post" action="">
 	<?php wp_nonce_field('wc1c-admin-settings-save', '_wc1c-admin-nonce'); ?>
-    <div class="wc1c-admin-settings wc1c-admin-connection rounded-3 bg-white p-2 mt-2">
+    <?php if($object->status) : ?>
+    <div class="wc1c-admin-settings wc1c-admin-connection bg-white rounded-3 mt-2 mb-2 px-2">
         <table class="form-table wc1c-admin-form-table wc1c-admin-settings-form-table">
-		    <?php $args['object']->generate_html($args['object']->get_fields(), true); ?>
+		    <?php $object->generate_html($object->get_fields(), true); ?>
         </table>
     </div>
-    <p class="submit">
+    <?php endif; ?>
+    <div class="submit p-0 mt-3">
 	    <?php
-            $connection_state = $args['object']->getSettings()->isConnected();
-            $button = __('Connect by Login & Password', 'wc1c');
-            if($connection_state)
+	        $button = __('Connect by WC1C site', 'wc1c');
+            if($object->status)
             {
                 $button = __('Disconnect', 'wc1c');
             }
         ?>
 
 	    <input type="submit" name="submit" id="submit" class="button button-primary" value="<?php echo $button; ?>">
-
-        <?php if(false === $connection_state) : ?>
-
-            <?php
-	            $current_url = home_url(add_query_arg($_GET));
-            ?>
-
-            <a href="<?php echo $args['object']->connection->buildUrl($current_url); ?>" class="button button-secondary"><?php _e('Connect by WC1C site', 'wc1c'); ?></a>
-        <?php endif; ?>
-    </p>
+    </div>
 </form>
