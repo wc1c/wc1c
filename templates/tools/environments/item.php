160dd5e818b9bde82a677a15dd749067c602bb8e
wc1c-ceo <ceo@wc1c.info>
2022-01-15 11:50:03 +0300

    Fix: more

--- templates/tools/environments/item.php
+++ templates/tools/environments/item.php
@@ -1,6 +1,6 @@
 <?php defined('ABSPATH') || exit; ?>
 
-<div class="mb-3 mt-2 pt-0 pb-0 pl-0 pr-0 bg-white">
+<div class="mb-3 mt-2 pt-0 pb-0 pl-0 pr-0" style=" border: 1px solid rgba(0,0,0,.125);">
     <div class="card-header p-2">
         <h2 class="m-0 p-0 pb-1"><?php _e($args['title']); ?></h2>
     </div>
