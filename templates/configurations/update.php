ea2e6a311806636d0a91040214429cbdbf7597b9
wc1c-ceo <ceo@wc1c.info>
2022-01-21 15:06:23 +0300

    Fix: more

--- templates/configurations/update.php
+++ templates/configurations/update.php
@@ -15,15 +15,6 @@
 
 <?php do_action(WC1C_ADMIN_PREFIX . 'before_configurations_update_show'); ?>
 
-<div class="row g-0">
-	<div class="col-24 col-lg-17">
-        <div class="pe-0 pe-lg-2">
-	        <?php do_action(WC1C_ADMIN_PREFIX . 'configurations_update_show'); ?>
-        </div>
-	</div>
-	<div class="col-24 col-lg-7">
-		<?php do_action(WC1C_ADMIN_PREFIX . 'configurations_update_sidebar_show'); ?>
-	</div>
-</div>
+<?php do_action(WC1C_ADMIN_PREFIX . 'configurations_update_show'); ?>
 
 <?php do_action(WC1C_ADMIN_PREFIX . 'after_configurations_update_show'); ?>
--- templates/configurations/update_form.php
+++ templates/configurations/update_form.php
@@ -1,13 +1,22 @@
 <?php defined('ABSPATH') || exit;?>
 
-<form method="post" action="<?php echo esc_url(add_query_arg('form', $args['object']->get_id())); ?>">
-	<?php wp_nonce_field('wc1c-admin-configurations-update-save', '_wc1c-admin-nonce'); ?>
-    <div class="bg-white p-2 rounded-3 wc1c-toc-container">
-        <table class="form-table wc1c-admin-form-table">
-            <?php $args['object']->generate_html($args['object']->get_fields(), true); ?>
-        </table>
+<div class="row g-0">
+    <div class="col-24 col-lg-17">
+        <div class="pe-0 pe-lg-2">
+            <form method="post" action="<?php echo esc_url(add_query_arg('form', $args['object']->get_id())); ?>">
+                <?php wp_nonce_field('wc1c-admin-configurations-update-save', '_wc1c-admin-nonce'); ?>
+                <div class="bg-white p-2 rounded-3 wc1c-toc-container">
+                    <table class="form-table wc1c-admin-form-table">
+                        <?php $args['object']->generate_html($args['object']->get_fields(), true); ?>
+                    </table>
+                </div>
+                <p class="submit">
+                    <input type="submit" name="submit" id="submit" class="button button-primary" value="<?php _e('Save configuration', 'wc1c'); ?>">
+                </p>
+            </form>
+        </div>
     </div>
-    <p class="submit">
-	    <input type="submit" name="submit" id="submit" class="button button-primary" value="<?php _e('Save configuration', 'wc1c'); ?>">
-    </p>
-</form>
+    <div class="col-24 col-lg-7">
+		<?php do_action(WC1C_ADMIN_PREFIX . 'configurations_update_sidebar_show'); ?>
+    </div>
+</div>
