e611e579f409340790dbaec38f38769e2a38f1a0
wc1c-ceo <ceo@wc1c.info>
2022-01-29 12:45:29 +0300

    Fix: more

---
+++ views/extensions/all.php
@@ -0,0 +1,20 @@
+<?php defined('ABSPATH') || exit; ?>
+
+<?php do_action('wc1c_admin_extensions_before_show'); ?>
+
+<div class="bg-white p-1 px-2 rounded-3 mt-2">
+	<?php
+        foreach($args['extensions'] as $extension_id => $extension_object)
+        {
+            $args =
+                [
+                    'id' => $extension_id,
+                    'object' => $extension_object
+                ];
+
+            wc1c()->views()->getView('extensions/item.php', $args);
+        }
+	?>
+</div>
+
+<?php do_action('wc1c_admin_extensions_after_show'); ?>
