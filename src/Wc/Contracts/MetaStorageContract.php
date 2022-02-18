b83a519759817482a24cb77965e544786b87c95d
wc1c-ceo <ceo@wc1c.info>
2022-02-18 18:03:26 +0300

    Fix: more

---
+++ src/Wc/Contracts/MetaStorageContract.php
@@ -0,0 +1,50 @@
+<?php namespace Wc1c\Wc\Contracts;
+
+defined('ABSPATH') || exit;
+
+use Wc1c\Wc\Abstracts\DataAbstract;
+
+/**
+ * MetaStorageContract
+ *
+ * @package Wc1c\Wc\Contracts
+ */
+interface MetaStorageContract
+{
+	/**
+	 * Returns an array of meta for an object
+	 *
+	 * @param DataAbstract $data Data object
+	 *
+	 * @return array
+	 */
+	public function readMeta(&$data);
+
+	/**
+	 * Deletes meta based on meta ID
+	 *
+	 * @param DataAbstract $data Data object
+	 * @param object $meta Meta an object (containing at least ->id)
+	 *
+	 * @return array
+	 */
+	public function deleteMeta(&$data, $meta);
+
+	/**
+	 * Add new piece of meta.
+	 *
+	 * @param DataAbstract $data Data object
+	 * @param object $meta Meta object (containing ->key and ->value)
+	 *
+	 * @return int meta ID
+	 */
+	public function addMeta(&$data, $meta);
+
+	/**
+	 * Update meta
+	 *
+	 * @param DataAbstract $data Data object
+	 * @param object $meta Meta object (containing ->id, ->key and ->value)
+	 */
+	public function updateMeta(&$data, $meta);
+}
