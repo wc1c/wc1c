04cb428b61b96d124e79dd31432e7764505cfc15
wc1c-ceo <ceo@wc1c.info>
2022-02-18 18:02:41 +0300

    Fix: more

---
+++ src/Wc/Contracts/StorageContract.php
@@ -0,0 +1,44 @@
+<?php namespace Wc1c\Wc\Contracts;
+
+defined('ABSPATH') || exit;
+
+use Wc1c\Wc\Abstracts\DataAbstract;
+
+/**
+ * StorageContract
+ *
+ * @package Wc1c\Wc\Contracts
+ */
+interface StorageContract
+{
+	/**
+	 * Method to create a new record of a Data based object
+	 *
+	 * @param DataAbstract $data Data object
+	 */
+	public function create(&$data);
+
+	/**
+	 * Method to read a record.
+	 *
+	 * @param DataAbstract $data Data object
+	 */
+	public function read(&$data);
+
+	/**
+	 * Updates a record in the database
+	 *
+	 * @param DataAbstract $data Data object
+	 */
+	public function update(&$data);
+
+	/**
+	 * Deletes a record from the database
+	 *
+	 * @param DataAbstract $data Data object
+	 * @param array $args Array of args to pass to the delete method
+	 *
+	 * @return bool result
+	 */
+	public function delete(&$data, $args = []);
+}
