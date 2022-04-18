2f80951fd3cc4162e9d1034a89e3bc2ccee4fea0
wc1c-ceo <ceo@wc1c.info>
2022-04-18 13:01:55 +0300

    Fix: more

--- views/helps/bugs.php
+++ views/helps/bugs.php
@@ -3,27 +3,25 @@
 <h2><?php _e( 'Found a bug?', 'wc1c' ); ?></h2>
 
 <p>
-	Прежде всего необходимо убедится - действительно ли была найдена ошибка и она до этого не была исправлена в обновлениях.
-    Если ошибка исправлена в обновлениях, нужно просто установить исправленную версию.
+    <?php _e('First of all, you need to make sure that a bug has been found and that it has not been fixed in updates before.', 'wc1c'); ?>
+	<?php _e('If the bug is fixed in the updates, you just need to install the corrected version.', 'wc1c'); ?>
 </p>
 <p>
-    Перед сообщением об ошибке, требуется проверить:
+	<?php _e('Before reporting an error need to check:', 'wc1c'); ?>
 </p>
 
 <ul>
-	<li>Правильно ли произведены настройки WordPress, WooCommerce, WC1C и расширений к ним.</li>
-    <li>Используются ли совместимые версии WordPress, WooCommerce, WC1C и расширений к ним. Совместимость можно посмотреть в разделе <b>Окружения</b>.</li>
+	<li><?php _e('Whether the settings for WordPress, WooCommerce, WC1C and their extensions are correct.', 'wc1c'); ?></li>
+    <li><?php _e('Whether compatible versions of WordPress, WooCommerce, WC1C and their extensions are used. Compatibility can be found in the Environments section.', 'wc1c'); ?></li>
 </ul>
 
 <p>
-    Если все настройки произведены верно и используются совместимые продукты свежих версий, но ошибка все равно присутствует - необходимо сообщить о ней.
+	<?php _e('If all settings are made correctly and compatible products of the latest versions are used, but the error is still present, you must report it.', 'wc1c'); ?>
+	<?php _e('Report a bug using the methods available to you. When reporting a bug, you must have a valid technical support code for the project on which the bug occurred.', 'wc1c'); ?>
 </p>
 
 <p>
-	<a href="#" class="button button-primary">
-		<?php _e( 'Report a bug', 'wc1c' ); ?>
-	</a>
-	<a href=" <?php echo admin_url( 'admin.php?page=wc1с&section=tools&tool_id=environments' ); ?>" class="button">
-		<?php _e( 'Environments', 'wc1c' ); ?>
+	<a href=" <?php echo admin_url('admin.php?page=wc1с&section=tools&tool_id=environments'); ?>" class="button">
+		<?php _e('Environments', 'wc1c'); ?>
 	</a>
 </p>
