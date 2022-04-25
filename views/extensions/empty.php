7755ac5497564e664a0e785a1c9364c29b415380
wc1c-ceo <ceo@wc1c.info>
2022-04-25 16:20:40 +0300

    Fix: more

--- views/extensions/empty.php
+++ views/extensions/empty.php
@@ -1,6 +1,15 @@
 <?php defined('ABSPATH') || exit; ?>
 
-<div class="wc1c-extensions-alert mb-2 mt-2">
+<div class="extensions-alert mb-2 mt-2">
     <h3><?php _e('Extensions not found.', 'wc1c'); ?></h3>
     <p><?php _e('As soon as the extensions are installed, they will appear in this section.', 'wc1c'); ?></p>
+
+	<?php
+	printf
+	(
+		'<p>%s %s</p>',
+		__('Information about all available official extensions is available on the website:', 'wc1c'),
+		'<a href="https://wc1c.info/extensions" target=_blank>https://wc1c.info/extensions</a>'
+	);
+	?>
 </div>
