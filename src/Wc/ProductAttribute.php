101a73489e4f6cc997cbe688f33dbd9ce8032ef4
wc1c-ceo <ceo@wc1c.info>
2022-06-09 00:41:35 +0300

    Init: ProductAttribute

---
+++ src/Wc/ProductAttribute.php
@@ -0,0 +1,15 @@
+<?php namespace Wc1c\Wc;
+
+defined('ABSPATH') || exit;
+
+use WC_Product_Attribute;
+
+/**
+ * ProductAttribute
+ *
+ * @package Wc1c\Wc
+ */
+class ProductAttribute extends WC_Product_Attribute
+{
+
+}
