b0c0e3383a361626b4b94f9d2510462fd6913f20
wc1c-ceo <ceo@wc1c.info>
2022-06-11 16:08:17 +0300

    Fix: more

--- views/configurations/empty.php
+++ views/configurations/empty.php
@@ -1,6 +1,16 @@
 <?php defined('ABSPATH') || exit;?>
 
-<h2><?php esc_html_e( 'Configurations not found.', 'wc1c' ); ?></h2>
+<h2><?php
+	if(!empty($_REQUEST['s']))
+	{
+		$search_text = wc_clean(wp_unslash($_REQUEST['s']));
+        printf('%s %s', __( 'Configurations by query is not found, query:', 'wc1c' ), $search_text);
+	}
+    else
+    {
+	    esc_html_e( 'Configurations not found.', 'wc1c' );
+    }
+?></h2>
 
 <a href="<?php echo $args['url_create']; ?>" class="mt-2 btn-lg d-inline-block page-title-action">
     <?php _e('New configuration', 'wc1c'); ?>
