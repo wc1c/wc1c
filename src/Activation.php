a2d0727c6e1b0e02f811724452c974cddfba07f8
wc1c-ceo <ceo@wc1c.info>
2022-07-03 21:17:49 +0300

    Fix: more

--- src/Activation.php
+++ src/Activation.php
@@ -18,6 +18,23 @@ final class Activation
 		if(false === get_option('wc1c_version', false))
 		{
 			update_option('wc1c_wizard', 'setup');
+
+			wc1c()->admin()->notices()->create
+			(
+				[
+					'id' => 'activation_welcome',
+					'dismissible' => false,
+					'type' => 'info',
+					'data' => __('WC1C successfully activated. You have made the right choice to integrate the site with 1C (plugin number one)!', 'wc1c'),
+					'extra_data' => sprintf
+					(
+						'<p>%s <a href="%s">%s</a></p>',
+						__('The basic plugin setup has not been done yet, so you can proceed to the setup, which takes no more than 5 minutes.', 'wc1c'),
+						admin_url('admin.php?page=wc1c'),
+						__('Go to setting.', 'wc1c')
+					)
+				]
+			);
 		}
 
 		if(false === get_option('wc1c_version_init', false))
