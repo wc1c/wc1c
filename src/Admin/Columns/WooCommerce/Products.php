50f4ee2ce088a8698092ae3e5901740d10aa1303
Oleg <109225168+frescoref@users.noreply.github.com>
2022-11-22 00:27:25 +0300

    Fix: 1C data in products list

--- src/Admin/Columns/WooCommerce/Products.php
+++ src/Admin/Columns/WooCommerce/Products.php
@@ -19,17 +19,19 @@ final class Products
 	public function __construct()
 	{
 		add_filter('manage_edit-product_columns',  [$this, 'manage_edit_product_columns']);
+		add_filter('wc1c_admin_interface_products_lists_column', [$this, 'wc1c_admin_interface_products_lists_column'], 10, 2);
+
 		add_action('manage_product_posts_custom_column', [$this, 'manage_product_posts_custom_column'], 10, 2);
 	}
 
 	/**
 	 * Adding a column to the list of products for displaying 1C information
 	 *
-	 * @param $columns
+	 * @param array $columns
 	 *
 	 * @return array
 	 */
-	public function manage_edit_product_columns($columns)
+	public function manage_edit_product_columns(array $columns): array
 	{
 		$columns_after =
 		[
@@ -55,21 +57,6 @@ final class Products
 			{
 				$content = apply_filters('wc1c_admin_interface_products_lists_column', $content, $post_id);
 			}
-			else
-			{
-				$schema_id = get_post_meta($post_id, '_wc1c_schema_id', true);
-				$config_id = get_post_meta($post_id, '_wc1c_configuration_id', true);
-
-				if($schema_id)
-				{
-					$content .= '<span class="na">' . __('Schema ID: ', 'wc1c') . $schema_id . '</span>';
-				}
-
-				if($config_id)
-				{
-					$content .= '<br/><span class="na">' . __('Configuration ID: ', 'wc1c')  . $config_id . '</span>';
-				}
-			}
 
 			if('' === $content)
 			{
@@ -78,5 +65,30 @@ final class Products
 
 			echo $content;
 		}
+	}
+
+	/**
+	 *
+	 * @param $content
+	 * @param $post_id
+	 *
+	 * @return string
+	 */
+	public function wc1c_admin_interface_products_lists_column($content, $post_id): string
+	{
+		$schema_id = get_post_meta($post_id, '_wc1c_schema_id', true);
+		$config_id = get_post_meta($post_id, '_wc1c_configuration_id', true);
+
+		if($schema_id)
+		{
+			$content .= '<span class="na">' . __('Schema ID: ', 'wc1c') . $schema_id . '</span>';
+		}
+
+		if($config_id)
+		{
+			$content .= '<br/><span class="na">' . __('Configuration ID: ', 'wc1c')  . $config_id . '</span>';
+		}
+
+		return $content;
 	}
 }
